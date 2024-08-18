106902a15a51c3a40b2e8ef34d8c8dfa0dca657f
Valeriy Petrikov <Val.pet0501@gmail.com>
2024-08-18 20:18:35 +0300

    fefe

--- live_testing.py
+++ live_testing.py
@@ -1,4 +1,29 @@
 import cv2
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models, transforms
+from torchvision.models import resnet18
+from torch.utils.data import DataLoader, TensorDataset
+from PIL import Image
+import pandas as pd 
+import os
+
+model = resnet18(pretrained=False)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 1)
+model.load_state_dict(torch.load("resnet18_finetuned.pth"))
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),  
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # https://stackoverflow.com/questions/58151507/why-pytorch-officially-use-mean-0-485-0-456-0-406-and-std-0-229-0-224-0-2
+])
+
+def PrintBeautyScore(image):
+    image = transform(image)
+    print(model(image.unsqueeze(0)).data[0][0])
+
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -8,16 +33,19 @@ if not cap.isOpened():
     print("bruh, dead camera")
     exit()
 
+
 while True:
     ret, frame = cap.read()
     
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    print(frame)
+    cv2.imshow('Face Detection', frame)   
 
-    cv2.imshow('Face Detection', frame)    
     if cv2.waitKey(1) == ord('q'):
         break
 
